6b024ecdb5cfc040227d291e5d1ceb5b055908de
kenta-crv <adp@crold-valieat.jp>
2020-09-09 19:47:00 +0900

    new

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def default_meta_tags
     {
-      site: "介護施設・老人ホームを知るなら｜介護の里",
+      site: "医療現場で働く情報なら｜メディワーカー",
       title: "<%= yield(:title) || eatery-japan %>" ,
-      description: "介護施設・老人ホームを知るなら介護の里",
-      keywords:    "介護施設,老人ホーム",
+      description: "医療現場で働く情報ならメディワーカー",
+      keywords:    "医療,ワーカー,メディカル",
       canonical: request.original_url,  # 優先されるurl
       charset: "UTF-8",
       #icon: [href: image_url('favicon.ico')]
